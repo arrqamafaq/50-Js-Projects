98ad384a787536b8a0ebb85c45108c35d14f695c
Arqam Afaq <arqamafaq03@gmail.com>
2025-01-10 11:44:07 +0530

    initial commit

---
+++ 17-Background-Image-Slider/app.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    url: "https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1596779845727-d88eb78a1b08?q=80&w=1033&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1432958576632-8a39f6b97dc7?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    url: "https://plus.unsplash.com/premium_photo-1669223464660-08f06bffabc0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1500622944204-b135684e99fd?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1275&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+
+
+
+
+
